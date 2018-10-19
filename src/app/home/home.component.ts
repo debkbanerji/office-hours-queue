cb880573d2e4cf20adcf871a078422c8210a61ef
deb <debkbanerji@gmail.com>
2018-10-19 00:33:31 -0400

    Sample keypress handling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,11 +9,24 @@ import {environment} from "../../environments/environment";
 export class HomeComponent implements OnInit {
 
     version = environment.VERSION;
+    maxGTIDLength = 9;
+
+    digitKeypressBuffer = [];
 
     constructor() {
     }
 
     ngOnInit() {
+        const component = this;
+        document.onkeypress = function (e) {
+            console.log(e.keyCode);
+            const detectedDigit = e.keyCode - 48;
+            if (detectedDigit >= 0 && detectedDigit <= 9) {
+                component.digitKeypressBuffer.push(detectedDigit.toString());
+                if (component.digitKeypressBuffer.length > component.maxGTIDLength) {
+                    component.digitKeypressBuffer.shift();
+                }
+            }
+        };
     }
-
 }
